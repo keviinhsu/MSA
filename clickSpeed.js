6a5ea526fdb78a8b0764810a3a35929e438eae43
kevin hsu <keviinhsu@gmail.com>
2016-09-03 11:51:17 +1200

    init commit

--- clickSpeed.js
+++ clickSpeed.js
@@ -1,19 +1,56 @@
 var canvas;
 var context;
+var drawArray = new Array();
 function animationFrame() {
     requestAnimationFrame(animationFrame);
     context.fillStyle = "black";
     context.fillRect(0, 0, 1280, 720);
+    for (var i = 0; i < drawArray.length, i++;) {
+        var d = drawArray[i];
+        d.drawButton();
+    }
 }
 function click(event) {
     var x = event.x;
     var y = event.y;
     alert('x =' + x + 'y = ' + y);
 }
+var button = (function () {
+    function button(x, y, width, height, text, fontSize) {
+        var _this = this;
+        if (fontSize === void 0) { fontSize = 32; }
+        this.drawButton = function () {
+            context.save();
+            context.beginPath();
+            context.textAlign = "center";
+            context.textBaseline = "middle";
+            context.fillStyle = "white";
+            context.font = _this.fontSize + "px Verdana";
+            context.fillText(_this.text, _this.x, _this.y);
+            context.restore();
+            context.save();
+            context.lineWidth = 2;
+            context.strokeStyle = "black";
+            context.rect(_this.x - _this.halfWidth, _this.y - _this.halfWidth, _this.width, _this.height);
+            context.stroke();
+            context.restore();
+        };
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.halfWidth = width / 2;
+        this.halfHeight = height / 2;
+        this.text = text;
+        this.fontSize = fontSize;
+    }
+    return button;
+}());
 window.onload = function () {
     canvas = document.getElementById('canvas');
     canvas.addEventListener("click", click, false);
     context = canvas.getContext("2d");
+    drawArray.push(new button(150, 100, 100, 200, "Click Me!"));
     animationFrame();
 };
 //# sourceMappingURL=clickSpeed.js.map
--- clickSpeed.ts
+++ clickSpeed.ts
@@ -1,10 +1,16 @@
 var canvas: HTMLCanvasElement;
 var context: CanvasRenderingContext2D;
+var drawArray: Array<button> = new Array<button>(); 
 
 function animationFrame(): void {
     requestAnimationFrame(animationFrame);
     context.fillStyle = "black";
     context.fillRect(0, 0, 1280, 720);
+
+    for (var i: number = 0; i < drawArray.length, i++;) {
+        var d: button = drawArray[i];
+        d.drawButton();
+    }
 }
 
 function click(event: MouseEvent): void {
@@ -14,9 +20,50 @@ function click(event: MouseEvent): void {
     alert('x =' + x + 'y = ' + y);
 }
 
+class button {
+    public x: number;
+    public y: number; 
+    public width: number;
+    public height: number; 
+    public halfWidth: number;
+    public halfHeight: number;
+    public text: string;
+    public fontSize: number;
+
+    constructor(x: number, y: number, width: number, height: number, text: string, fontSize: number = 32) {
+        this.x = x; 
+        this.y = y; 
+        this.width = width; 
+        this.height = height;
+        this.halfWidth = width / 2;
+        this.halfHeight = height / 2;
+        this.text = text;
+        this.fontSize = fontSize; 
+    }
+
+    public drawButton = (): void => {
+        context.save();
+        context.beginPath();
+        context.textAlign = "center";
+        context.textBaseline = "middle";
+        context.fillStyle = "white";
+        context.font = this.fontSize + "px Verdana";
+        context.fillText(this.text, this.x, this.y);
+        context.restore();
+
+        context.save();
+        context.lineWidth = 2;
+        context.strokeStyle = "black";
+        context.rect(this.x - this.halfWidth, this.y - this.halfWidth, this.width, this.height); 
+        context.stroke();
+        context.restore();
+    }
+}
+
 window.onload = () => {
     canvas = <HTMLCanvasElement>document.getElementById('canvas');
     canvas.addEventListener("click", click, false);
     context = canvas.getContext("2d");
+    drawArray.push(new button(150, 100, 100, 200, "Click Me!"))
     animationFrame();
 }
