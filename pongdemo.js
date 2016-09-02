420a120429fa1af4bd537aae5ee83214f583dc0c
kevin hsu <keviinhsu@gmail.com>
2016-09-02 16:14:03 +1200

    init commit

--- pongdemo.js
+++ pongdemo.js
@@ -1,12 +1,39 @@
 var canvas; //Bring up the canvas
-var context; //Render in 2D
+var context; //Object rendered in 2D
+var Circle = (function () {
+    function Circle(x, y, radius, color, lineWidth) {
+        var _this = this;
+        if (x === void 0) { x = 0; }
+        if (y === void 0) { y = 0; }
+        if (radius === void 0) { radius = 10; }
+        if (lineWidth === void 0) { lineWidth = 2; }
+        this.draw = function () {
+            context.beginPath();
+            context.strokeStyle = _this.color;
+            context.lineWidth = _this.lineWidth;
+            context.arc(_this.x, _this.y, _this.radius, 0, 2 * Math.PI);
+            context.stroke();
+        };
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.lineWidth = lineWidth;
+    }
+    return Circle;
+}());
 function gameLoop() {
     requestAnimationFrame(gameLoop); //Calls the gameloop everytime the frame refreshes
     context.fillStyle = "black"; //Colours the canvas black
     context.fillRect(0, 0, 1280, 720); //The dimension of the canvas
 }
+function mouseDown(event) {
+    var x = event.x - canvas.offsetLeft;
+    var y = event.y - canvas.offsetTop;
+}
 window.onload = function () {
     canvas = document.getElementById("canvas");
+    canvas.addEventListener("mouseDown", mouseDown, false);
     context = canvas.getContext("2d");
     gameLoop();
     // Grabbing the element and putting it on the canvas
--- pongdemo.ts
+++ pongdemo.ts
@@ -1,5 +1,28 @@
 var canvas: HTMLCanvasElement; //Bring up the canvas
-var context: CanvasRenderingContext2D; //Render in 2D
+var context: CanvasRenderingContext2D; //Object rendered in 2D
+
+class Circle { 
+    public x: number;
+    public y: number;
+    public radius: number; 
+    public lineWidth: number; 
+    public color: string; 
+    constructor(x: number = 0, y: number = 0, radius: number = 10, color: string, lineWidth: number = 2) {
+        this.x = x; 
+        this.y = y;
+        this.radius = radius; 
+        this.color = color;
+        this.lineWidth = lineWidth; 
+    } 
+    public draw =(): void => {
+        context.beginPath();
+        context.strokeStyle = this.color;
+        context.lineWidth = this.lineWidth;
+        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        context.stroke();
+    }
+}
+
 
 function gameLoop(){
     requestAnimationFrame(gameLoop); //Calls the gameloop everytime the frame refreshes
@@ -7,8 +30,16 @@ function gameLoop(){
     context.fillRect(0, 0, 1280, 720); //The dimension of the canvas
 }
 
+function mouseDown(event: MouseEvent): void {
+    var x: number = event.x - canvas.offsetLeft;
+    var y: number = event.y - canvas.offsetTop;
+
+    
+}
+
 window.onload = () => {
     canvas = <HTMLCanvasElement>document.getElementById("canvas");
+    canvas.addEventListener("mouseDown", mouseDown, false);
     context = canvas.getContext("2d");
     gameLoop();
     // Grabbing the element and putting it on the canvas
